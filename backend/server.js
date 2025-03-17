30518fcf8dada5449e71794718a00baa4aaf0d09
Hein Htut Zaw <henry@Heins-MacBook-Pro.local>
2025-03-17 22:15:41 +0900

    Added cross dependency packages

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.listen(PORT, ()=>{
 })
 
 const __dirname = path.resolve();
-
+console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
     app.get('*', (req, res) => {
