7182ad5b1a94c1722e99dec283ca0525224b411b
Dowon Kim <dowon.kim@stonybrook.edu>
2025-03-19 15:24:39 +0900

    Add temporary input fields

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,7 +1,24 @@
 import './App.css'
+import { Editable, EditableInput, EditablePreview } from '@chakra-ui/react'
 
 function Signup() {
-    return <h1>signup header</h1>
+    return (
+        <div>
+            <h1>signup header</h1>
+            <Editable placeholder='Username'>
+                <EditablePreview />
+                <EditableInput />
+            </Editable>
+            <Editable placeholder='Email'>
+                <EditablePreview />
+                <EditableInput />
+            </Editable>
+            <Editable placeholder='Password'>
+                <EditablePreview />
+                <EditableInput />
+            </Editable>
+        </div>
+    )
 }
 
 export default Signup;
