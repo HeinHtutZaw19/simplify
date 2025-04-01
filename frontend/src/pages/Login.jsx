4ed6cacf3779e1bc231ae7b02de4533a37407a18
Dowon Kim <dowon.kim@stonybrook.edu>
2025-04-01 19:54:50 +0900

    Add base code for login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react'
+import { Input, Button } from '@chakra-ui/react'
+
 function Login() {
-    return <h1>login header</h1>
+    const [loginInfo, setLoginInfo] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setLoginInfo((state) => ({
+            ...state,
+            [name]: value
+        }));
+    };
+
+    const onLoginClick = async () => {
+        console.log('login clicked');
+    }
+
+    return (
+        <div>
+            <h1>login header</h1>
+            <Input placeholder='Username' name='username' value={loginInfo.username} onChange={handleChange} />
+            <Input type='password' placeholder='Password' name='password' value={loginInfo.password} onChange={handleChange} />
+            <Button onClick={onLoginClick}>Log in</Button>
+        </div>
+    )
 }
 
 export default Login;
