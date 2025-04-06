08980a3d0d866ce58cb4f0adf759c6bfba3cd788
Dowon Kim <dowon.kim@stonybrook.edu>
2025-04-06 20:46:18 +0900

    Add base code for skinlabs page

--- frontend/src/pages/SkinLabPage.jsx
+++ frontend/src/pages/SkinLabPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import {Flex, Text} from '@chakra-ui/react'
+import { Flex, Text, Image } from '@chakra-ui/react'
+import skinanalysis from '../assets/skinanalysis.png';
 
 const SkinLabPage = () => {
   return (
-    <Flex width={"100%"}>
-        <Text>
-            Skin Lab Page
-        </Text>
+    <Flex width={"100%"} direction="row" justify="center">
+      <Image
+        src={skinanalysis}
+        alt="skin analysis"
+        height="400px"
+        mt="100px"
+        border="20px solid"
+        rounded="20px"
+        borderColor="blue.400"
+      />
     </Flex>
   )
 }
