cee513d7521db168a4083533266b2e9db1d98576
Hein Htut Zaw <heinhtutzawhenry530@gmail.com>
2025-04-13 16:38:42 +0900

    finished responsiveness

--- frontend/src/components/LeaderPodium.jsx
+++ frontend/src/components/LeaderPodium.jsx
@@ -3,27 +3,26 @@ import { FaCrown } from "react-icons/fa"
 
 const LeaderboardPodium = ({ first, second, third }) => {
     return (
-        <Flex justify="center" align="end" gap={4}>
-
-            {console.log("LeaderboardPodium", { first, second, third })}
+        <Flex justify="center" align="end" gap={{ base: 2, md: 4 }}>
+            {/* 2nd Place */}
             <Box
                 bg="gray.800"
                 p={4}
-                pt={12}
-                pb={6}
+                pt={{ base: 10, md: 12 }}
+                pb={{ base: 4, md: 6 }}
                 borderRadius="2xl"
                 textAlign="center"
                 position="relative"
-                w="120px"
-                h="180px"
+                w={{ base: "80px", md: "100px", lg: "120px" }}
+                h={{ base: "140px", md: "160px", lg: "180px" }}
             >
                 <Avatar
-                    size="xl"
+                    size="lg"
                     name={second.name}
                     src={second.image}
                     border="4px solid pink"
                     position="absolute"
-                    top="-70px"
+                    top="-50px"
                     left="50%"
                     transform="translateX(-50%)"
                 />
@@ -36,13 +35,14 @@ const LeaderboardPodium = ({ first, second, third }) => {
                     color="white"
                     borderRadius="full"
                     px={2}
+                    fontSize={{ base: "xs", md: "sm" }}
                 >
                     2
                 </Badge>
-                <Text mt={8} fontWeight="medium" color="white">
+                <Text mt={8} fontWeight="medium" fontSize={{ base: "sm", md: "md" }} color="white">
                     {second.name}
                 </Text>
-                <Text fontWeight="bold" fontSize="xl" color="pink.400">
+                <Text fontWeight="bold" fontSize={{ base: "xs", md: "sm" }} color="pink.400">
                     {second.points}
                 </Text>
             </Box>
@@ -51,21 +51,21 @@ const LeaderboardPodium = ({ first, second, third }) => {
             <Box
                 bg="gray.900"
                 p={4}
-                pt={14}
-                pb={6}
+                pt={{ base: 12, md: 14 }}
+                pb={{ base: 4, md: 6 }}
                 borderRadius="2xl"
                 textAlign="center"
                 position="relative"
-                w="140px"
-                h="220px"
+                w={{ base: "100px", md: "120px", lg: "140px" }}
+                h={{ base: "180px", md: "200px", lg: "220px" }}
             >
                 <Avatar
-                    size="xl"
+                    size="lg"
                     name={first.name}
                     src={first.image}
                     border="4px solid green"
                     position="absolute"
-                    top="-60px"
+                    top="-50px"
                     left="50%"
                     transform="translateX(-50%)"
                 />
@@ -73,10 +73,10 @@ const LeaderboardPodium = ({ first, second, third }) => {
                     as={FaCrown}
                     color="yellow.400"
                     position="absolute"
-                    top="-90px"
+                    top={{ base: "-70px", md: "-75px" }}
                     left="50%"
                     transform="translateX(-50%)"
-                    boxSize={6}
+                    boxSize={{ base: 4, md: 6 }}
                 />
                 <Badge
                     position="absolute"
@@ -87,13 +87,14 @@ const LeaderboardPodium = ({ first, second, third }) => {
                     color="white"
                     borderRadius="full"
                     px={2}
+                    fontSize={{ base: "xs", md: "sm" }}
                 >
                     1
                 </Badge>
-                <Text mt={10} fontWeight="medium" color="white">
+                <Text mt={10} fontWeight="medium" fontSize={{ base: "xs", md: "sm" }} color="white">
                     {first.name}
                 </Text>
-                <Text fontWeight="bold" fontSize="2xl" color="green.300">
+                <Text fontWeight="bold" fontSize={{ base: "xs", md: "sm" }} color="green.300">
                     {first.points}
                 </Text>
             </Box>
@@ -102,21 +103,21 @@ const LeaderboardPodium = ({ first, second, third }) => {
             <Box
                 bg="gray.800"
                 p={4}
-                pt={12}
-                pb={6}
+                pt={{ base: 10, md: 12 }}
+                pb={{ base: 4, md: 6 }}
                 borderRadius="2xl"
                 textAlign="center"
                 position="relative"
-                w="120px"
-                h="160px"
+                w={{ base: "80px", md: "100px", lg: "120px" }}
+                h={{ base: "130px", md: "150px", lg: "160px" }}
             >
                 <Avatar
-                    size="xl"
+                    size="lg"
                     name={third.name}
                     src={third.image}
                     border="4px solid orange"
                     position="absolute"
-                    top="-70px"
+                    top="-50px"
                     left="50%"
                     transform="translateX(-50%)"
                 />
@@ -129,13 +130,14 @@ const LeaderboardPodium = ({ first, second, third }) => {
                     color="white"
                     borderRadius="full"
                     px={2}
+                    fontSize={{ base: "xs", md: "sm" }}
                 >
                     3
                 </Badge>
-                <Text mt={8} fontWeight="medium" color="white">
+                <Text mt={8} fontWeight="medium" fontSize={{ base: "xs", md: "sm" }} color="white">
                     {third.name}
                 </Text>
-                <Text fontWeight="bold" fontSize="xl" color="orange.400">
+                <Text fontWeight="bold" fontSize={{ base: "xs", md: "sm" }} color="orange.400">
                     {third.points}
                 </Text>
             </Box>
